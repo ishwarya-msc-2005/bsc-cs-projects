f989f3090a72787b815c41e7240736ebe41d7ac4
ishwarya-msc-2005 <ishwaryap077@gmail.com>
2026-06-12 14:25:31 +0530

    Day3-first laptop java program

---
+++ Day3Laptop.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Day3Laptop {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("En peru enna da?");
+        String peru = input.nextLine();
+
+        System.out.println("Vanakkam " + peru + "! Nee laptop la Java odavachita! 👑🔥");
+
+        input.close();
+    }
+}
